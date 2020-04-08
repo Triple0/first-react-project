9928a4d928b09601aeb8a4cd267b71e13153d4ee
Triple0 <lekan.olapeju@yahoo.com>
2020-04-08 22:34:54 +0100

    first commit on first react project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Button, Icon } from 'semantic-ui-react';
+import ListExampleIcon from './link.js';
+import FormExampleFieldControlId from './form-extract.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hello, World</h1>
+        <FormExampleFieldControlId />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          I'm adding some text!
+        </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +27,14 @@ function App() {
         >
           Learn React
         </a>
+        <Button size="small" color="green">
+          <Icon name="download" />
+          download
+        </Button>
+
+        <Button size="small" icon={{color: 'red', name: 'like'}} content ="like" />
+
+        <ListExampleIcon />
       </header>
     </div>
   );
